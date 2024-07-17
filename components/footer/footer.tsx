0301123a91bb0bce508b7abad958f85e1131deac
andron13 <andron13@gmail.com>
2024-07-17 13:44:38 +0200

    feat: update libraries 2

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -5,7 +5,7 @@ import { CONTACT_PHONE_ONE, CONTACT_PHONE_TWO } from '@/shared/constants';
 const Footer = () => {
   return (
     <footer className="flex w-full items-center justify-center gap-3 border-t-2 border-t-borderGrey pt-2 text-sm font-bold sm:gap-[175px] sm:text-base md:pt-4 lg:gap-[360px] lg:text-lg">
-      <address className="flex items-center gap-4 ">
+      <address className="flex items-center gap-4">
         <PhoneIcon className="h-5 w-5 lg:h-7 lg:w-7" />
         <a href="tel:">Mob. {CONTACT_PHONE_TWO}</a>
         <a href="tel:">Tel. {CONTACT_PHONE_ONE}</a>
