1db502615a5224b3f958b3d525f9d0ea56a3f190
andron13 <andron13@gmail.com>
2025-04-15 20:07:38 +0200

    fix: minor issues

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -4,11 +4,9 @@ export const SITE_URL = 'https://alema-consulting.de/';
 export const CONTACT_PHONE_ONE = '03085629694';
 export const CONTACT_PHONE_TWO = '01703707021';
 
-
-export const matrix: string = '/assets/Matrix.svg';
+export const matrix: string = '/assets/matrix.svg';
 
 export const logo: string = '/assets/logo-triangle.svg';
-
 
 export const defaultMetaObj = {
 	title: {
